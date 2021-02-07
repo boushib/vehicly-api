8d63d2fa8180494e2d3d4b292b89ae69e8d34009
boushib <boushib01@gmail.com>
2021-02-07 14:19:14 +0100

    map the API result into DTO

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using vehiclesStoreAPI.DTO;
 using vehiclesStoreAPI.Models;
 using vehiclesStoreAPI.Repositories;
 
@@ -11,24 +13,26 @@ namespace vehiclesStoreAPI.Controllers
   public class VehiclesController : ControllerBase
   {
     private readonly IVehicleRepository _repository;
+    private readonly IMapper _mapper;
 
     // use stor shortcut to create a constructor.
-    public VehiclesController(IVehicleRepository repository)
+    public VehiclesController(IVehicleRepository repository, IMapper mapper)
     {
       _repository = repository;
+      _mapper = mapper;
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<Vehicle>> GetVehicles()
+    public ActionResult<IEnumerable<VehicleDTO>> GetVehicles()
     {
-      return Ok(_repository.GetVehicles());
+      return Ok(_mapper.Map<IEnumerable<VehicleDTO>>(_repository.GetVehicles()));
     }
 
     [HttpGet("{id}")]
-    public ActionResult<Vehicle> GetVehicleById(string id)
+    public ActionResult<VehicleDTO> GetVehicleById(string id)
     {
       var vehicle = _repository.GetVehicleById(id);
-      return vehicle == null ? NotFound() : Ok(vehicle);
+      return vehicle == null ? NotFound() : Ok(_mapper.Map<VehicleDTO>(vehicle));
     }
   }
 }
