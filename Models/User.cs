d8b69035f264d34c2268839394546cabf6fb67d8
boushib <boushib01@gmail.com>
2021-02-10 23:03:13 +0100

    remove dupplicate JSON properties

--- Models/User.cs
+++ Models/User.cs
@@ -8,15 +8,11 @@ namespace vehiclesStoreAPI.Models
   public class User
   {
     [Key]
-    [JsonPropertyName("id")]
     public Guid Id { get; set; }
     [Required]
-    [JsonPropertyName("username")]
     public string Username { get; set; }
     [Required]
-    [JsonPropertyName("password")]
     public string Password { get; set; }
-    [JsonPropertyName("roles")]
     public List<string> Roles { get; set; } = new List<string>() { "user" };
   }
 }
--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -8,26 +8,18 @@ namespace vehiclesStoreAPI.Models
   {
     // use prop shortcut to create a new property
     [Key]
-    [JsonPropertyName("id")]
     public Guid Id { get; set; }
-    [JsonPropertyName("model")]
     public string Make { get; set; }
     [Required]
-    [JsonPropertyName("model")]
     public string Model { get; set; }
     [Required]
-    [JsonPropertyName("fuel")]
     public string Fuel { get; set; }
     [Required]
-    [JsonPropertyName("year")]
     public int Year { get; set; }
     [Required]
-    [JsonPropertyName("createdAt")]
     public int Price { get; set; }
     [Required]
-    [JsonPropertyName("createdAt")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    [JsonPropertyName("updatedAt")]
     public DateTime? UpdatedAt { get; set; }
   }
 }
