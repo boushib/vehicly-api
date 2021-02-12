a5ddbe8a1f6a5c8e5ed4add161f8e42764f9f4b2
boushib <boushib01@gmail.com>
2021-02-12 16:48:32 +0100

    run migrations

---
+++ Migrations/20210212153453_VehicleLocationAndImage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vehiclesStoreAPI.Migrations
+{
+    public partial class VehicleLocationAndImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Vehicles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Vehicles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Vehicles");
+        }
+    }
+}
